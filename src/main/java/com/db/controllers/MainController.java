654cc2b8bdb95a6ca14d0912b21309160890b143
Liagush <48795444+Liagush@users.noreply.github.com>
2019-06-24 23:30:40 +0300

    Вывод законов на страницу по категориям не правильный. Реализация поиска по нескольким сущностям закончена. Начал делать главную страницу. Вывод выдачи сделан, но надо добавить выдачу по типу сущности, так же исправить стили для выдачи. Есть проблема когда добавляешь текст закона он обрезается

--- src/main/java/com/db/controllers/MainController.java
+++ src/main/java/com/db/controllers/MainController.java
@@ -1,10 +1,13 @@
 package com.db.controllers;
 import com.db.model.LawArticle;
 import com.db.repos.LawArticleRepo;
+import com.db.search.classes.HibernateSearchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -13,13 +16,20 @@ public class MainController {
     @Autowired
     private LawArticleRepo lawArticleRepo;
 
+    @Autowired
+    private HibernateSearchService hibernateSearchService;
+
     @GetMapping("/main")
-    public String main(Map<String,Object> model) {
+    public String main(String q, Map<String,Object> model) {
 
         Iterable<LawArticle> listOfLaws = lawArticleRepo.findAll();
         model.put("listOfLaws", listOfLaws);
 
+        if(q != null) {
+            List searchResults = hibernateSearchService.search(q);
+            model.put("searchResults", searchResults);
+        }
+
         return "main";
     }
-
 }
