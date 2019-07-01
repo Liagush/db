d9738adacd65428b177e37a8e5b5cbbc409db0a7
Liagush <48795444+Liagush@users.noreply.github.com>
2019-07-01 17:32:56 +0300

    Вывод законов на страницу по категориям не правильный. не работает авторизация и регистрация, не запускается приложение из за ошибок. Исправил миграцию. И добавил классы WebSecurityConfig и WebApplication

--- src/main/java/com/db/auth/service/SecurityServiceImpl.java
+++ src/main/java/com/db/auth/service/SecurityServiceImpl.java
@@ -3,6 +3,7 @@ package com.db.auth.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
--- src/main/resources/db/migration/V3__Users_and_roles.sql
+++ src/main/resources/db/migration/V3__Users_and_roles.sql
@@ -1,3 +1,9 @@
+
+
+--/////////////////////////////////////////--
+
+insert into hibernate_sequence values ( 1 );
+
 create table role (
     id bigint not null auto_increment,
     name varchar(50),
@@ -5,16 +11,13 @@ create table role (
 ) engine=innoDB;
 
 create table user (
-    id bigint not null auto_increment,
-    email varchar(50) not null,
-    password varchar(100) not null,
+    id bigint not null,
+    email varchar(50),
+    password varchar(50),
     resolution bit not null,
-    username varchar(50) not null,
+    username varchar(50),
     primary key (id)
 ) engine=innoDB;
-
-insert into hibernate_sequence values ( 1 );
-insert into hibernate_sequence values ( 1 );
 
 create table user_roles (
     users_id bigint not null,
@@ -31,45 +34,5 @@ alter table user_roles
     add constraint user_roles_users_fk
     foreign key (users_id)
     references user (id);
-
-
---/////////////////////////////////////////--
-
--- create table hibernate_sequence (
---     next_val bigint
--- ) engine=innoDB;
---
--- insert into hibernate_sequence values ( 1 );
---
--- create table role (
---     id bigint not null auto_increment,
---     name varchar(50),
---     primary key (id)
--- ) engine=innoDB;
---
--- create table user (
---     id bigint not null,
---     email varchar(50),
---     password varchar(50),
---     resolution bit not null,
---     username varchar(50),
---     primary key (id)
--- ) engine=innoDB;
---
--- create table user_roles (
---     users_id bigint not null,
---     roles_id bigint not null,
---     primary key (users_id, roles_id)
--- ) engine=innoDB;
---
--- alter table user_roles
---     add constraint user_roles_roles_fk
---     foreign key (roles_id)
---     references role (id);
---
--- alter table user_roles
---     add constraint user_roles_users_fk
---     foreign key (users_id)
---     references user (id);
 
 
