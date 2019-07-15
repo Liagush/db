25b20782462fd123a364f88f3a36bb26f043f169
Liagush <48795444+Liagush@users.noreply.github.com>
2019-07-16 00:50:20 +0300

    Вывод законов на страницу по категориям не правильный. добавил вывод даты в правильном формате в adminpage

--- src/main/java/com/db/auth/model/User.java
+++ src/main/java/com/db/auth/model/User.java
@@ -1,6 +1,10 @@
 package com.db.auth.model;
 
 import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
 import java.util.Set;
 
 @Entity
@@ -109,5 +113,12 @@ public class User {
         this.dateOfRegistration = dateOfRegistration;
     }
 
+    public String getLocalDateOfRegistration() {
 
+        LocalDateTime date = LocalDateTime.ofEpochSecond(dateOfRegistration,  0, ZoneOffset.UTC);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMM yyyy");
+        String text = date.format (formatter);
+
+        return text;
+    }
 }
